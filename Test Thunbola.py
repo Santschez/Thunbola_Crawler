d8a84d8f09f294d1199a6d5de67e0c41b4909e40
Santschez <31672001+Santschez@users.noreply.github.com>
2017-09-07 21:58:11 +0200

    Add files via upload

---
+++ Test Thunbola.py
@@ -0,0 +1,483 @@
+###Type in Input Results HERE
+results = "24049" 
+
+###Results Week 1
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche1.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 1 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 2
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche2.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+ccode = content.find(results) ###line to type in numbers
+
+print 'Results Week 2 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 3
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche3.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 3 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 4
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche4.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 4 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 5
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche5.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 5 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 6
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche6.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 6 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 7
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche7.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 7 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 8
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche8.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 8 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 9
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche9.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 9 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 10
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche10.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 10 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 11
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche11.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 11 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 12
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche12.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 12 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 13
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche13.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 13 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 14
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche14.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 14 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 15
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche15.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 15 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 16
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche16.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 16 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 17
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche17.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 17 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 18
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche18.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 18 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 19
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche19.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 19 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 20
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche20.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 20 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 21
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche21.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 21 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 22
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche22.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 22 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 23
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche23.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 23 | if result is -1 you are a LOSER!!!!'
+print code
+
+###Results Week 24
+import urllib2,cookielib
+
+site = "http://thunbola.ch/woche24.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',}
+
+req = urllib2.Request(site, headers=hdr)
+
+try:
+    page = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
+
+content = page.read()
+code = content.find(results) ###line to type in numbers
+
+print 'Results Week 24 | if result is -1 you are a LOSER!!!!'
+print code
+
